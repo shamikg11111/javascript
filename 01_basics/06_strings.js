9adf9c6fd6f81774b09a2e7e644a227b417f801c
shamikg11111 <shamikg11@gmail.com>
2025-01-10 22:33:16 +0000

    config container done 6

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,8 @@
+// an old fashioned way of concatenating strings
+const name="shamik"
+const rep=10
+
+console.log(name + rep )
+
+//a better readable way - using backticks (string interpolation)
+console.log(`hello my name is ${name} and ive done ${rep} reps`)
